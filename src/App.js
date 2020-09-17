283edd809ffbe6bb9634fa1cdb6fe99467749444
waw177699 <waw177699@alibaba-inc.com>
2020-09-17 19:38:26 +0800

    up

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [cdStatus, setCdStatus] = useState(-1)
   const [record, setRecord] = useState([])
   const [name, setName] = useState('')
+  const [red, setRed] = useState(0)
 
   useEffect(() => {
     setCd(parseInt(Math.random() * 8) + 3)
@@ -27,35 +28,49 @@ function App() {
       return
     }
     if (cdStatus) {
+      const finalCD = cd
       cdInterval = setInterval(() => {
-        setCd(prev => (prev - 0.01).toFixed(2))
+        setCd(prev => {
+          if (prev > 0) {
+            const color = ((finalCD - prev) / finalCD) * 255
+            setRed(color)
+          } else {
+            setRed(255)
+          }
+          return (prev - 0.01).toFixed(2)
+        })
       }, 10);
     } else {
-      setRecord(prev => [...prev, { cd, name }].sort((a, b) => Math.abs(a.cd) - Math.abs(b.cd)))
+      setRecord(prev => [...prev, { cd, name, red }].sort((a, b) => Math.abs(a.cd) - Math.abs(b.cd)))
+      setRed(0)
       setCd(parseInt(Math.random() * 8) + 3)
       clearInterval(cdInterval)
     }
   }, [cdStatus])
   return (
-    <div className="App">
-      <h1>Aiwen's Site!</h1>
+    <div className="App" style={{ background: `rgba(255, ${255 - red}, ${255 - red})` }}>
       {
         performance && loadTime ?
           <div>
-            Load Time: {loadTime}ms
+            Page Load Time: {loadTime}ms
           </div>
           :
           null
       }
-      <h2>Your Name</h2>
+      <h3>Your Name</h3>
       <input value={name} onChange={e => setName(e.target.value)} />
-      <h2>Little Game</h2>
-      <div>{cd}</div>
-      <div onClick={() => setCdStatus(prev => prev === -1 ? true : !prev)}>{(!cdStatus || cdStatus === -1) ? 'Start' : 'Stop'}</div>
-      <h2>Record</h2>
+      <h3>Count Down Game</h3>
+      <h1 style={{ color: `rgb(${red}, 0, 0)` }}>{cd}</h1>
+      <div
+        className="cd-button"
+        onClick={() => setCdStatus(prev => prev === -1 ? true : !prev)}
+      >
+        {(!cdStatus || cdStatus === -1) ? 'Start' : 'Stop'}
+      </div>
+      <h3>Record</h3>
       {
         record.length ?
-          record.map(r => <h3>{r.cd} - {r.name || 'No Name'}</h3>) :
+          record.map((r, i) => <h3 key={i}>{i + 1}. {r.name || 'No Name'} - <span style={{ color: `rgba(${r.red}, 0, 0)` }}>{r.cd}</span></h3>) :
           null
       }
     </div>
