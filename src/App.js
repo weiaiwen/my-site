152decd2a424c3ad87f8880ebfdd09a90bff3c11
waw177699 <waw177699@alibaba-inc.com>
2020-09-17 16:07:04 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const nEntris = performance && performance.getEntries('navigation')[0]
+
 function App() {
   return (
     <div className="App">
-      Aiwen's Site!
+      <h1>Aiwen's Site!</h1>
+      {
+        performance ?
+          <div>
+            Load Time: {nEntris.loadEventEnd}
+          </div>
+          :
+          null
+      }
     </div>
   );
 }
